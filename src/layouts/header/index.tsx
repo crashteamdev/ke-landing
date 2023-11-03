dd285a0d7b25b3084f892c912c66418fd100aeeb
Dmitry Trofimov <dimkaboin@yandex.ru>
2023-11-04 02:42:54 +0300

    add top bar

--- src/layouts/header/index.tsx
+++ src/layouts/header/index.tsx
@@ -12,81 +12,84 @@ interface IProps {
 const Header: React.FC = ({ headerLayout }: IProps) => {
     const [burger, setBurger] = useState(false);
     return (
-        <header className="header">
-            <div className="container">
-                <div className="row header-row">
-                    <div className='header-left'>
-                        <a href="/" className="header-logo">
-                            <img src="./images/logo.svg" alt="" />
-                        </a>
-                        <div className="header-menu">
-                            <a className='active' href="/">Главная</a>
-                            <Link
-                                activeClass="active"
-                                to="tariff"
-                                spy={true}
-                                smooth={true}
-                                offset={-70}
-                                duration={500}
-                            >
-                                Тарифы
-                            </Link>
-                            <Link
-                                activeClass="active"
-                                to="about"
-                                spy={true}
-                                smooth={true}
-                                offset={0}
-                                duration={500}
-                            >
-                                О нас
-                            </Link>
-                            <a target='_blank' rel="noreferrer" href="https://vk.cc/coPhwU">
-                                Расширение
-                            </a>
-                        </div>
-                    </div>
-                    <div className="header-right">
-                        <a className='header-phone' target='_blank' rel="noreferrer" href="https://t.me/marketdbru">
-                            <TelegramIcon color="#fff" />
-                            Telegram
-                        </a>
-                        <a className='btn' href="https://lk.marketdb.ru/">Войти</a>
-                    </div>
-                    <div className="header-burger" onClick={() => setBurger(true)}>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                    </div>
-                </div>
-            </div>
-            {burger &&
-                <div className="header-mob-menu">
-                    <div>
-                        <div className="header-mob-menu-top">
+        <>
+            <div className='top-bar'><a href="https://t.me/marketdbru/51" target='_blank'>Приглашаем на бесплатный вебинар «Знакомство с MarketDB» 5 ноября.</a></div>
+            <header className="header">
+                <div className="container">
+                    <div className="row header-row">
+                        <div className='header-left'>
                             <a href="/" className="header-logo">
-                                <img src="./images/logo-mob.svg" alt="" />
+                                <img src="./images/logo.svg" alt="" />
                             </a>
-                            <div onClick={() => setBurger(false)}>
-                                <CloseIcon color='#1A1A1A' />
+                            <div className="header-menu">
+                                <a className='active' href="/">Главная</a>
+                                <Link
+                                    activeClass="active"
+                                    to="tariff"
+                                    spy={true}
+                                    smooth={true}
+                                    offset={-70}
+                                    duration={500}
+                                >
+                                    Тарифы
+                                </Link>
+                                <Link
+                                    activeClass="active"
+                                    to="about"
+                                    spy={true}
+                                    smooth={true}
+                                    offset={0}
+                                    duration={500}
+                                >
+                                    О нас
+                                </Link>
+                                <a target='_blank' rel="noreferrer" href="https://vk.cc/coPhwU">
+                                    Расширение
+                                </a>
                             </div>
                         </div>
-                        <div className="header-mob-menu-content">
-                            <a href="/">Главная</a>
-                            <a href="/">Тарифы</a>
-                            <a href="/">О нас</a>
+                        <div className="header-right">
+                            <a className='header-phone' target='_blank' rel="noreferrer" href="https://t.me/marketdbru">
+                                <TelegramIcon color="#fff" />
+                                Telegram
+                            </a>
+                            <a className='btn' href="https://lk.marketdb.ru/">Войти</a>
+                        </div>
+                        <div className="header-burger" onClick={() => setBurger(true)}>
+                            <span></span>
+                            <span></span>
+                            <span></span>
                         </div>
                     </div>
-                    <div className="header-mob-menu-bottom">
-                        <a target='_blank' rel="noreferrer" href="https://t.me/marketdbru" className='header-mob-menu-phone'>
-                            <TelegramIcon color="#fff" />
-                            Telegram
-                        </a>
-                        <a target='_blank' rel="noreferrer" href="https://lk.marketdb.ru/" className="btn">Войти</a>
-                    </div>
                 </div>
-            }
-        </header>
+                {burger &&
+                    <div className="header-mob-menu">
+                        <div>
+                            <div className="header-mob-menu-top">
+                                <a href="/" className="header-logo">
+                                    <img src="./images/logo-mob.svg" alt="" />
+                                </a>
+                                <div onClick={() => setBurger(false)}>
+                                    <CloseIcon color='#1A1A1A' />
+                                </div>
+                            </div>
+                            <div className="header-mob-menu-content">
+                                <a href="/">Главная</a>
+                                <a href="/">Тарифы</a>
+                                <a href="/">О нас</a>
+                            </div>
+                        </div>
+                        <div className="header-mob-menu-bottom">
+                            <a target='_blank' rel="noreferrer" href="https://t.me/marketdbru" className='header-mob-menu-phone'>
+                                <TelegramIcon color="#fff" />
+                                Telegram
+                            </a>
+                            <a target='_blank' rel="noreferrer" href="https://lk.marketdb.ru/" className="btn">Войти</a>
+                        </div>
+                    </div>
+                }
+            </header>
+        </>
     )
 };
 
