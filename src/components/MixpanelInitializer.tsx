bdd19a5a6997269fa96e072c45920baa6a050d43
vitaxa <vitaxa93gamebox@gmail.com>
2024-11-10 17:46:56 +0300

    change to cookie

--- src/components/MixpanelInitializer.tsx
+++ src/components/MixpanelInitializer.tsx
@@ -7,9 +7,8 @@ export default function MixpanelInitializer() {
     useEffect(() => {
         if (typeof window !== "undefined") {
             mixpanel.init("c15d57d13fcbfc5f41170407d2aa82b0", {
-                debug: true,
                 track_pageview: true,
-                persistence: "localStorage",
+                persistence: "cookie",
             });
         }
     }, []);
