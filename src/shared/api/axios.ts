ae3870e0ed359e842f81b5c188bb826e79734e1f
vitaxa <vitaxa93gamebox@gmail.com>
2025-11-11 20:23:26 +0300

    strapi v4 -> v5 compatibility

--- src/shared/api/axios.ts
+++ src/shared/api/axios.ts
@@ -1,6 +1,10 @@
 import axios from "axios";
 
 export const axiosApi = axios.create({
-    baseURL: 'https://strapi.marketdb.pro/api/'
+    baseURL: 'https://strapi.marketdb.pro/api/',
     // baseURL: 'http://localhost:1337/api/'
+    headers: {
+        // Enable Strapi v4 response format compatibility
+        'Strapi-Response-Format': 'v4'
+    }
 });
